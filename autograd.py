f8c3bab4abcb82101363a693181e433eb832f6c1
Void User <amshulha@ualberta.ca>
2026-04-17 22:10:44 -0600

    more autograd

--- autograd.py
+++ autograd.py
@@ -1,5 +1,6 @@
 # https://docs.pytorch.org/tutorials/beginner/blitz/autograd_tutorial.html
 import torch
+from torch import nn
 from torchvision.models import resnet18, ResNet18_Weights
 model = resnet18(weights=ResNet18_Weights.DEFAULT)
 print(f'Model parameters {model}')
@@ -13,3 +14,20 @@ loss = (prediction - labels).sum()
 # sgd with momentum
 optim = torch.optim.SGD(model.parameters(),lr=1e-2,momentum=0.9)
 optim.step()
+
+x = torch.tensor([-1.,3.],requires_grad=True)
+y = torch.tensor([6., 7.],requires_grad=True)
+z = torch.tensor([-1., 4.],requires_grad=True)
+Q = x**2+y**2-z**2
+external_grad = torch.tensor([1., 1.])
+Q.backward(gradient=external_grad)
+print(x.grad)
+
+# resnet freeze
+
+for param in model.parameters():
+    param.requires_grad = False
+model.fc = nn.Linear(512,10)
+optimizer = torch.optim.SGD(model.parameters(),lr=1e-2,momentum=0.9)
+optim.step()
+
