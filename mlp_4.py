702b7421d77daabe69bad0e93b66a7e9ce93ff71
Void User <amshulha@ualberta.ca>
2026-04-28 11:29:49 -0600

    update neuron code

--- mlp_4.py
+++ mlp_4.py
@@ -7,33 +7,38 @@ from entropy import BCE
 def sigmoid(x):
     return 1 / (1+np.e**(-x))
 def accuracy(y,y_pred): 
-    acc = (y_pred==y).sum()
-    acc /= len(y_pred)
+    acc = (y==y_pred).mean()
     return acc
 
 def return_labels(z):
     ### given output of sigmoid classifier, convert to 0 or 1 label
-    pass
+    return z.argmax(1)
+
+
 
 class Neuron(object):
     def __init__(self,n_in,n_out):
         self.weights = np.random.normal(size=(n_in,n_out))
-        self.bias = np.random.normal(size=(1,n_out))
+        #self.bias = np.random.zeros(size=(1,n_out))
+        self.bias = np.zeros((1,n_out))
         self.waits = np.vstack([self.weights,self.bias])
         self.dW = np.zeros_like(self.waits)
 
     def forward(self, inputs):
         dummy = np.ones((inputs.shape[0],1))
         # numerical stabilizer of sigmoid compute
-        inputs -= np.max(inputs)
         self.design = np.column_stack([inputs,dummy])
-        self.z = sigmoid(self.design.dot(self.waits))
+        #self.design -= np.max(self.design)
+        scores = self.design.dot(self.waits)
+        scores -= np.max(scores,axis=1)[:,None]
+        self.z = sigmoid(scores)
         return self.z
 
     def backward(self,dD):
         dZ = dD*self.z*(1-self.z)
         self.dW += self.design.T@dZ
         return self.dW
+
     def update(self, lr=1e-2):
         # gradient descent update
         self.waits -= lr*self.dW
@@ -47,37 +52,62 @@ class Neuron(object):
         loss += BCE(y,y_hat)
         return loss
 
-n = 10 # number of data points
-p = 3  # number of features
+    def predict(self, X): 
+        dummy = np.ones((X.shape[0],1))
+        # numerical stabilizer of sigmoid compute
+        design = np.column_stack([X,dummy])
+        scores = design.dot(self.waits)
+        scores -= np.max(scores,axis=1)[:,None]
+        z = sigmoid(scores)
+        y_pred = return_labels(z)
+        return y_pred
+        
+
+
+n = 5000 # number of data points
+p = 10  # number of features
 c = 2   # number of classes
 X = np.random.normal(size=(n,p)) # synthetic data
 y = np.random.randint(0,c,size=(n,1)) # binaru class
+# selecting a random batch size of data
+batch_size = np.random.randint(0,n)
+mask = np.random.choice(batch_size, n, replace=True)
+X_batch = X[mask]
+y_batch = y[mask]
+
 model = Neuron(p,c) #classifier neuron
 
+
 ### gradient descent
-num_epochs = 200
+num_epochs = 1000
 a = range(num_epochs)
 inits = 0.
 loss = 0.
-lr = 0.01
-loss_dict = {}
-loss_dict = loss_dict.fromkeys(a,inits)
+lr = 1e-3
+#loss_dict = {}
+#loss_dict = loss_dict.fromkeys(a,inits)
+loss_hist= [] 
 for epoch in range(num_epochs):
     acc = 0.
     forward = model.forward(X)
-    dL = forward - y
+    dL = (forward - y)
     backward = model.backward(dL)
     loss = model.loss(y)
-    loss_dict[epoch]+=loss
+    loss_hist.append(loss)
+    #loss_dict[epoch]+=loss
     if epoch % 100 == 0:
         print(f"Epoch {epoch} loss {loss}")
         ### implement accuracy
+        acc += accuracy(model.predict(X),y)
         print(f"Accuracy {acc}")
     model.update(lr=lr)
     model.zero_grad_()
 
 
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+plt.plot(loss_hist)
+plt.savefig('mlp_loss_graph.png')
 #
 #for k in loss_dict:
 #    losses = loss_dict[k]
